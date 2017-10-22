8fd44f07bffa0123e4768c1142bf3b2a6d9db910
Thomas Leyh <leyht@informatik.uni-freiburg.de>
2017-10-22 22:54:51 +0200

    More fine tuning and especially error handling

--- schema.sql
+++ schema.sql
@@ -1,45 +1,43 @@
 CREATE TABLE posts (
-  id  integer PRIMARY KEY,
-  created_at timestamp without time zone,
-  uploader_id integer NOT NULL,
-  score integer DEFAULT 0 NOT NULL,
-  source character varying DEFAULT ''::character varying NOT NULL,
-  md5 character varying NOT NULL,
-  rating character(1) DEFAULT 'q'::bpchar NOT NULL,
-  image_width integer NOT NULL,
+  id           integer PRIMARY KEY,
+  created_at   timestamp without time zone,
+  updated_at   timestamp without time zone,
+  uploader_id  integer NOT NULL,
+  score        integer DEFAULT 0 NOT NULL,
+  source       character varying DEFAULT ''::character varying NOT NULL,
+  md5          character varying NOT NULL,
+  rating       character(1) DEFAULT 'q'::bpchar NOT NULL,
+  image_width  integer NOT NULL,
   image_height integer NOT NULL,
-  fav_count integer DEFAULT 0 NOT NULL,
-  file_ext character varying NOT NULL,
-  parent_id integer,
+  file_ext     character varying NOT NULL,
+  parent_id    integer,
   has_children boolean DEFAULT false NOT NULL,
-  file_size integer NOT NULL,
-  --fav_string
-  --pool_string
-  up_score integer DEFAULT 0 NOT NULL,
-  down_score integer DEFAULT 0 NOT NULL,
-  is_banned boolean DEFAULT false NOT NULL,
-  pixiv_id integer,
-  bit_flags bigint DEFAULT 0 NOT NULL,
-  --tag_string_artist
-  --tag_string_character
-  --tag_string_copyright
-  --tag_string_general
-  file_url character varying DEFAULT ''::character varying NOT NULL
+  file_size    integer NOT NULL,
+  up_score     integer DEFAULT 0 NOT NULL,
+  down_score   integer DEFAULT 0 NOT NULL,
+  is_pending   boolean DEFAULT false NOT NULL,
+  is_flagged   boolean DEFAULT false NOT NULL,
+  is_deleted   boolean DEFAULT false NOT NULL,
+  is_banned    boolean DEFAULT false NOT NULL,
+  pixiv_id     integer,
+  bit_flags    bigint DEFAULT 0 NOT NULL,
+  file_url     character varying DEFAULT ''::character varying NOT NULL,
+  scraped_at   timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE tags (
-	id serial PRIMARY KEY,
-	name character varying NOT NULL UNIQUE,
-	category character(1) DEFAULT 'g'::bpchar NOT NULL
+	id         serial PRIMARY KEY,
+	name       character varying NOT NULL UNIQUE,
+	category   character(1) DEFAULT 'g'::bpchar NOT NULL
 );
 
 CREATE TABLE tagged (
-	tag_id integer REFERENCES tags (id),
-	post_id integer REFERENCES posts (id),
+	tag_id     integer REFERENCES tags (id),
+	post_id    integer REFERENCES posts (id),
   PRIMARY KEY (tag_id, post_id)
 );
 
-CREATE TABLE pools (
+CREATE TABLE pooled (
 	pool_id integer,
 	post_id integer REFERENCES posts (id),
   PRIMARY KEY (pool_id, post_id)
--- scrapedbooru.go
+++ scrapedbooru.go
@@ -9,11 +9,11 @@ import (
 	"net/http"
 	"os"
 	"os/user"
+	"strconv"
 	"strings"
 	"time"
-
+	"errors"
 	_ "github.com/lib/pq"
-	"strconv"
 )
 
 const (
@@ -46,9 +46,20 @@ type dbConf struct {
 	Database string
 }
 
+// Return URL to database from configuration.
+func (conf *dbConf) getUrl() string {
+	return fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable",
+		conf.User, conf.Password, conf.Host, conf.Port, conf.Database)
+}
+
+const postColNames = "id, created_at, updated_at, uploader_id, score, source, md5, rating, image_width, image_height, " +
+	"file_ext, parent_id, has_children, file_size, up_score, down_score, " +
+	"is_pending, is_flagged, is_deleted, is_banned, pixiv_id, bit_flags, file_url"
+
 type Post struct {
 	Id                 int    `json:"id"`
 	CreatedAt          string `json:"created_at"`
+	UpdatedAt          string `json:"updated_at"`
 	UploaderId         int    `json:"uploader_id"`
 	Score              int    `json:"score"`
 	Source             string `json:"source"`
@@ -56,7 +67,6 @@ type Post struct {
 	Rating             string `json:"rating"`
 	ImageWidth         int    `json:"image_width"`
 	ImageHeight        int    `json:"image_height"`
-	FavCount           int    `json:"fav_count"`
 	FileExt            string `json:"file_ext"`
 	ParentId           int    `json:"parent_id"`
 	HasChildren        bool   `json:"has_children"`
@@ -65,9 +75,12 @@ type Post struct {
 	PoolString         string `json:"pool_string"`
 	UpScore            int    `json:"up_score"`
 	DownScore          int    `json:"down_score"`
+	IsPending          bool   `json:"is_pending"`
+	IsFlagged          bool   `json:"is_flagged"`
+	IsDeleted          bool   `json:"is_deleted"`
 	IsBanned           bool   `json:"is_banned"`
 	PixivId            int    `json:"pixiv_id"`
-	BitFlags           int    `json:"bit_flags"`
+	BitFlags           int64  `json:"bit_flags"`
 	TagStringArtist    string `json:"tag_string_artist"`
 	TagStringCharacter string `json:"tag_string_character"`
 	TagStringCopyright string `json:"tag_string_copyright"`
@@ -75,143 +88,203 @@ type Post struct {
 	FileUrl            string `json:"file_url"`
 }
 
-func (conf *dbConf) getUrl() string {
-	return fmt.Sprintf("postgresql://%s:%s@%s:%d/%s?sslmode=disable",
-		conf.User, conf.Password, conf.Host, conf.Port, conf.Database)
-}
-
 // Don't forget to call res.Body.Close()
-func makeRequest(url string, client *http.Client, auth *authDbooru) *http.Response {
+func makeRequest(url string, client *http.Client, auth *authDbooru) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 	if auth != nil {
 		req.SetBasicAuth(auth.Login, auth.ApiKey)
 	}
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalln(err)
+		return nil, err
 	}
 	if res.StatusCode != 200 {
-		log.Fatalln(res.Status)
-	} else if res.StatusCode == 429 {
-		// TODO Maybe first wait some time and try again.
-		log.Fatalln(res.Status)
+		return nil, errors.New("Response status indicates failure: " + res.Status)
 	}
-	return res
+	return res, nil
 }
 
 func dbInsertTags(tags string, category string, postId int, db *sql.DB) {
-	// TODO Do nothing if tags are empty.
-	for _, t := range strings.Split(tags, " ") {
-		_, err := db.Exec("INSERT INTO tags(name, category) VALUES ($1, $2)",
-			t, category)
-		if err != nil {
-			log.Println(err)
+	// Do nothing is there are not tags.
+	if s := strings.TrimSpace(tags); s == "" {
+		return
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("Inserting tags failed for post: %d (%s)", postId, err)
+		return
+	}
+	tagSplit := strings.Split(tags, " ")
+	tagInsert, err := tx.Prepare("INSERT INTO tags(name, category) VALUES ($1, $2) ON CONFLICT DO NOTHING")
+	if err != nil {
+		log.Printf("Inserting tags failed for post: %d (%s)", postId, err)
+	} else {
+		for _, t := range tagSplit {
+			_, err := tagInsert.Exec(t, category)
+			if err != nil {
+				log.Printf("Could not insert tag %s for post: %d (%s)", t, postId, err)
+			}
 		}
-		var tagId int
-		err = db.QueryRow("SELECT id FROM tags WHERE name = $1", t).Scan(&tagId)
-		if err != nil {
-			log.Fatalln(err)
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Printf("Inserting tags failed for post: %d (%s)", postId, err)
+		return
+	}
+	// Start new transaction for building many-to-many relationship.
+	tx, err = db.Begin()
+	if err != nil {
+		log.Printf("Inserting tags failed for post: %d (%s)", postId, err)
+		return
+	}
+	tagQuery, errQ := tx.Prepare("SELECT id FROM tags WHERE name = $1")
+	taggedInsert, errI := tx.Prepare("INSERT INTO tagged VALUES ($1, $2)")
+	if errQ != nil || errI != nil {
+		log.Printf("Inserting tags failed for post: %d (%s; %s)", postId, errQ, errI)
+	} else {
+		for _, t := range tagSplit {
+			var tagId int
+			err := tagQuery.QueryRow(t).Scan(&tagId)
+			if err != nil {
+				log.Printf("Querying tag %s failed for post: %d (%s)", t, postId, err)
+			}
+			_, err = taggedInsert.Exec(tagId, postId)
+			if err != nil {
+				log.Printf("Creating relationship for tag %d and post %d failed (%s)", tagId, postId, err)
+			}
 		}
-		_, err = db.Exec("INSERT INTO tagged VALUES ($1, $2)", tagId, postId)
-		if err != nil {
-			log.Fatalln(err)
-		}
+	}
+	err = tx.Commit()
+	if err != nil {
+		log.Printf("Inserting tags failed for post: %d (%s)", postId, err)
+		return
 	}
 }
 
 func dbInsert(p *Post, db *sql.DB) {
 	// First insert the post itself.
-	_, err := db.Exec("INSERT INTO posts VALUES "+
-		"($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20)",
-		p.Id, p.CreatedAt, p.UploaderId, p.Score, p.Score, p.Md5, p.Rating, p.ImageWidth, p.ImageHeight,
-		p.FavCount, p.FileExt, p.ParentId, p.HasChildren, p.FileSize, p.UpScore, p.DownScore,
-		p.IsBanned, p.PixivId, p.BitFlags, p.FileUrl)
+	_, err := db.Exec("INSERT INTO posts VALUES"+
+		"($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23)",
+		p.Id, p.CreatedAt, p.UpdatedAt, p.UploaderId, p.Score, p.Source, p.Md5, p.Rating, p.ImageWidth, p.ImageHeight,
+		p.FileExt, p.ParentId, p.HasChildren, p.FileSize, p.UpScore, p.DownScore,
+		p.IsPending, p.IsFlagged, p.IsDeleted, p.IsBanned, p.PixivId, p.BitFlags, p.FileUrl)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf("Could not insert post: %d (%s)", p.Id, err)
+		return
 	}
 	// Then try to insert the tags.
 	dbInsertTags(p.TagStringArtist, artist, p.Id, db)
 	dbInsertTags(p.TagStringCharacter, character, p.Id, db)
 	dbInsertTags(p.TagStringCopyright, copyright, p.Id, db)
 	dbInsertTags(p.TagStringGeneral, general, p.Id, db)
-	// Insert favString.
-	for _, fav := range strings.Split(p.FavString, " ") {
-		userId, err := strconv.Atoi(strings.TrimPrefix(fav, "fav:"))
-		if err == nil {
-			_, err = db.Exec("INSERT INTO favorites VALUES ($1, $2)", userId, p.Id)
-			if err != nil {
-				log.Fatalln(err)
+	// Start transaction for inserting favorites and pools.
+	if strings.TrimSpace(p.FavString) == "" &&
+		strings.TrimSpace(p.PoolString) == "" {
+		return
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		log.Printf("Inserting favorites and pools failed for post: %d (%s)", p.Id, err)
+		return
+	}
+	stmt, err := tx.Prepare("INSERT INTO favorites VALUES ($1, $2)")
+	if err != nil {
+		log.Printf("Inserting favorites failed for post: %d (%s)", p.Id, err)
+	} else {
+		for _, fav := range strings.Split(p.FavString, " ") {
+			userId, err := strconv.Atoi(strings.TrimPrefix(fav, "fav:"))
+			if err == nil {
+				_, err = stmt.Exec(userId, p.Id)
+				if err != nil {
+					log.Printf("Could not insert favorite %d for post: %d (%s)", userId, p.Id, err)
+				}
 			}
 		}
 	}
-	// Insert poolStrings.
-	for _, pool := range strings.Split(p.PoolString, " ") {
-		poolId, err := strconv.Atoi(strings.TrimPrefix(pool, "pool:"))
-		if err == nil {
-			_, err = db.Exec("INSERT INTO pools VALUES ($1, $2)", poolId, p.Id)
-			if err != nil {
-				log.Fatalln(err)
+	stmt, err = tx.Prepare("INSERT INTO pooled VALUES ($1, $2)")
+	if err != nil {
+		log.Printf("Inserting pools failed for post: %d (%s)", p.Id, err)
+	} else {
+		for _, pool := range strings.Split(p.PoolString, " ") {
+			poolId, err := strconv.Atoi(strings.TrimPrefix(pool, "pool:"))
+			if err == nil {
+				_, err = stmt.Exec(poolId, p.Id)
+				if err != nil {
+					log.Printf("Could not insert pool %d for post: %d (%s)", poolId, p.Id, err)
+				}
 			}
 		}
 	}
+	tx.Commit() // Actually commit transaction of favorites and pools.
 }
 
 // Save the contents of post.FileUrl in current directory.
-func saveFile(post *Post, client *http.Client) {
-	// TODO Check if FileUrl field exists.
+func saveFile(post *Post, client *http.Client) error {
+	if post.FileUrl == "" {
+		return errors.New("There is no FileUrl field for post: " + strconv.Itoa(post.Id))
+	}
 	file, err := os.Create(fmt.Sprintf("%d.%s", post.Id, post.FileExt))
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	defer file.Close()
-	res := makeRequest(netloc+post.FileUrl, client, nil)
+	res, err := makeRequest(netloc+post.FileUrl, client, nil)
+	if err != nil {
+		return err
+	}
 	defer res.Body.Close()
 	_, err = io.Copy(file, res.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
+	return nil
 }
 
 // Parse specified JSON file from config directory
 // and write contents to corresponding struct v.
-func parseConfig(path string, v interface{}) {
-	// TODO Return error instead of exiting.
+func parseConfig(path string, v interface{}) error {
 	usr, err := user.Current()
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	file, err := os.Open(usr.HomeDir + "/" + configDir + "/" + path)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
 	defer file.Close()
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&v)
 	if err != nil {
-		log.Fatalln(err)
+		return err
 	}
+	return nil
 }
 
 // Query for all posts with startId <= postId < stopId.
 // There is a hard limit (from the server) for a limit of 20 posts.
 // Optionally use authentication if account credentials are given.
 func requestPost(startId int, stopId int, client *http.Client, auth *authDbooru) []Post {
-	// TODO Include post ID in logging.
 	if stopId-startId > dbooruLimit {
-		log.Fatalln("The hart limit for requesting posts is 20.")
+		log.Fatalf("The hard limit for requesting posts is 20. %d posts actually requested.",
+			stopId-startId)
 	}
-	query := fmt.Sprintf("?tags=id:<%d", stopId)
+	query := fmt.Sprintf("?tags=id:<%d&limit=%d", stopId, dbooruLimit)
 	url := netloc + "/" + netpath + query
-	res := makeRequest(url, client, auth)
+	var p []Post
+	res, err := makeRequest(url, client, auth)
+	if err != nil {
+		log.Printf("An error occured when requesting: %s (%s)", url, err)
+		return p
+	}
 	defer res.Body.Close()
 	decoder := json.NewDecoder(res.Body)
-	var p []Post
-	err := decoder.Decode(&p)
+	err = decoder.Decode(&p)
 	if err != nil {
-		log.Fatalln(err)
+		log.Printf("Failed decoding response of: %s (%s)", url, err)
+		return p
 	}
 	// Filter p so there are no posts with id < startId
 	var pLen = len(p)
@@ -224,28 +297,44 @@ func requestPost(startId int, stopId int, client *http.Client, auth *authDbooru)
 	return p[:pLen]
 }
 
+func openDatabase(dbc *dbConf) (*sql.DB, error) {
+	db, err := sql.Open(driverName, dbc.getUrl())
+	if err != nil {
+		return nil, err
+	}
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, err
+}
+
 func main() {
 	client := &http.Client{
 		Timeout: clientTimeout,
 	}
 	var auth authDbooru
-	parseConfig("auth.json", &auth)
-	p := requestPost(0, 20, client, &auth)
+	err := parseConfig("auth.json", &auth)
+	if err != nil {
+		log.Fatalf("Could not open configuration file: $HOME/%s/auth.json (%s)",
+			configDir, err)
+	}
+	p := requestPost(20, 40, client, &auth)
 	for _, v := range p {
 		fmt.Print(v.Id, " ")
 	}
 
 	var dbc dbConf
-	parseConfig("database.json", &dbc)
-	db, err := sql.Open(driverName, dbc.getUrl())
+	err = parseConfig("database.json", &dbc)
 	if err != nil {
-		log.Fatalln(err)
+		log.Fatalf("Could not open configuration file: $HOME/%s/database.json (%s)",
+			configDir, err)
 	}
-	if err := db.Ping(); err != nil {
-		log.Fatalln(err)
+	db, err := openDatabase(&dbc)
+	if err != nil {
+		log.Fatal(err)
 	}
-
 	for i := range p {
 		dbInsert(&p[i], db)
+		//saveFile(&p[i], client)
 	}
 }
